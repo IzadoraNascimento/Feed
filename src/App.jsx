357b53157c3c87e021bd6ce7116dd9860100ed37
IzadoraNascimento <izasnascimento@icloud.com>
2023-07-26 22:28:06 -0300

    create-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Forms from "./components/Forms";
+import Submit from "./components/Submit";
 
 function App() {
 
   return (
     <>
     <Forms />
+    <Submit />
     </>
   )
 }
---
+++ src/components/Submit/index.jsx
@@ -0,0 +1,3 @@
+export default function Submit() {
+  return <button className="submit">Enviar Comentário</button>
+}
