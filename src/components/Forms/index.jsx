e94b6d2d7110363026461fe2d99173b8d7edfde1
IzadoraNascimento <izasnascimento@icloud.com>
2023-07-28 00:30:26 -0300

    text - Seção de Comentários

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -3,6 +3,11 @@ export default function Forms() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Seção de Comentários
+            </h2>
+          </div>
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
